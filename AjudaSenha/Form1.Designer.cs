e85d1c4a977a1ae978eab13b49f3a8d6282649d7
yrmeww <59840433+yrmeww@users.noreply.github.com>
2020-07-18 05:48:29 -0300

    Add files via upload

---
+++ AjudaSenha/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace AjudaSenha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_1 = new System.Windows.Forms.PictureBox();
+            this.pb_2 = new System.Windows.Forms.PictureBox();
+            this.pb_3 = new System.Windows.Forms.PictureBox();
+            this.pb_4 = new System.Windows.Forms.PictureBox();
+            this.pb_5 = new System.Windows.Forms.PictureBox();
+            this.tB_senha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pb_Close = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Close)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_1
+            // 
+            this.pb_1.BackColor = System.Drawing.Color.White;
+            this.pb_1.BackgroundImage = global::AjudaSenha.Properties.Resources._0;
+            this.pb_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_1.Location = new System.Drawing.Point(107, 141);
+            this.pb_1.Name = "pb_1";
+            this.pb_1.Size = new System.Drawing.Size(100, 100);
+            this.pb_1.TabIndex = 0;
+            this.pb_1.TabStop = false;
+            // 
+            // pb_2
+            // 
+            this.pb_2.BackColor = System.Drawing.Color.White;
+            this.pb_2.BackgroundImage = global::AjudaSenha.Properties.Resources._0;
+            this.pb_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_2.Location = new System.Drawing.Point(224, 141);
+            this.pb_2.Name = "pb_2";
+            this.pb_2.Size = new System.Drawing.Size(100, 100);
+            this.pb_2.TabIndex = 1;
+            this.pb_2.TabStop = false;
+            // 
+            // pb_3
+            // 
+            this.pb_3.BackColor = System.Drawing.Color.White;
+            this.pb_3.BackgroundImage = global::AjudaSenha.Properties.Resources._0;
+            this.pb_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_3.Location = new System.Drawing.Point(342, 141);
+            this.pb_3.Name = "pb_3";
+            this.pb_3.Size = new System.Drawing.Size(100, 100);
+            this.pb_3.TabIndex = 2;
+            this.pb_3.TabStop = false;
+            // 
+            // pb_4
+            // 
+            this.pb_4.BackColor = System.Drawing.Color.White;
+            this.pb_4.BackgroundImage = global::AjudaSenha.Properties.Resources._0;
+            this.pb_4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_4.Location = new System.Drawing.Point(463, 141);
+            this.pb_4.Name = "pb_4";
+            this.pb_4.Size = new System.Drawing.Size(100, 100);
+            this.pb_4.TabIndex = 3;
+            this.pb_4.TabStop = false;
+            // 
+            // pb_5
+            // 
+            this.pb_5.BackColor = System.Drawing.Color.White;
+            this.pb_5.BackgroundImage = global::AjudaSenha.Properties.Resources._0;
+            this.pb_5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_5.Location = new System.Drawing.Point(581, 141);
+            this.pb_5.Name = "pb_5";
+            this.pb_5.Size = new System.Drawing.Size(100, 100);
+            this.pb_5.TabIndex = 4;
+            this.pb_5.TabStop = false;
+            // 
+            // tB_senha
+            // 
+            this.tB_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tB_senha.Location = new System.Drawing.Point(116, 261);
+            this.tB_senha.Name = "tB_senha";
+            this.tB_senha.Size = new System.Drawing.Size(565, 80);
+            this.tB_senha.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(311, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 59);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Testar!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pb_Close
+            // 
+            this.pb_Close.BackColor = System.Drawing.Color.Crimson;
+            this.pb_Close.BackgroundImage = global::AjudaSenha.Properties.Resources._02_07_Emergency_Exit_Button_02;
+            this.pb_Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb_Close.Location = new System.Drawing.Point(707, 12);
+            this.pb_Close.Name = "pb_Close";
+            this.pb_Close.Size = new System.Drawing.Size(49, 48);
+            this.pb_Close.TabIndex = 7;
+            this.pb_Close.TabStop = false;
+            this.pb_Close.Click += new System.EventHandler(this.pb_Close_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::AjudaSenha.Properties.Resources.BG;
+            this.ClientSize = new System.Drawing.Size(768, 530);
+            this.ControlBox = false;
+            this.Controls.Add(this.pb_Close);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tB_senha);
+            this.Controls.Add(this.pb_5);
+            this.Controls.Add(this.pb_4);
+            this.Controls.Add(this.pb_3);
+            this.Controls.Add(this.pb_2);
+            this.Controls.Add(this.pb_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Close)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_1;
+        private System.Windows.Forms.PictureBox pb_2;
+        private System.Windows.Forms.PictureBox pb_3;
+        private System.Windows.Forms.PictureBox pb_4;
+        private System.Windows.Forms.PictureBox pb_5;
+        private System.Windows.Forms.TextBox tB_senha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pb_Close;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ AjudaSenha/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AjudaSenha
+{
+    public partial class Form1 : Form
+    {
+        public string[] mensagens = loadText();
+        public int[] testesCheck = { 0, 0, 0, 0, 0, 0 };
+        System.Drawing.Image[] imgs = { AjudaSenha.Properties.Resources._1, AjudaSenha.Properties.Resources._2, AjudaSenha.Properties.Resources._3, AjudaSenha.Properties.Resources._4, AjudaSenha.Properties.Resources._5 };
+        public PictureBox[] cores = new PictureBox[5];
+        public string RYpath = "";
+        public Boolean teste = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("Para que este programa funcione corretamente, é necessário um arquivo chamado `rockyou.txt`, disponibilizado no gitHub.");
+            MessageBox.Show("Abra o arquivo na janela à seguir.");
+
+            openFileDialog1.Title = "Abrir arquivo RockYou";
+            openFileDialog1.Filter = "txt files(*.txt)| *.txt";
+            openFileDialog1.ShowDialog();
+            RYpath = openFileDialog1.FileName;
+            teste = checkRY();
+
+
+            
+        }
+
+
+        public void execTestes(string senha)
+        {
+
+            bool[] testes = new bool[5];
+            testes[0] = testeUm(senha);
+            testes[1] = testeDois(senha);
+            testes[2] = testeTres(senha);
+            testes[3] = testeQuatro(senha);
+            testes[4] = testeCinco(senha);
+
+            cores[0] = pb_1;
+            cores[1] = pb_2;
+            cores[2] = pb_3;
+            cores[3] = pb_4;
+            cores[4] = pb_5;
+
+            if (testes[0] == false)
+            {
+                readMessage(36, 38);
+                return;
+            }
+
+            updateCheckColors(testes);
+            afterTest(testes);
+
+        }
+
+        public Boolean checkRY()
+        {
+            StreamReader sr = new StreamReader(RYpath);
+            string linha = sr.ReadLine();
+            if (!linha.Equals("123456"))
+            {
+                MessageBox.Show("ERRO! Este arquivo não corresponde ao ROCKYOU.txt, baixe o arquivo novamente e tente outra vez!","Erro",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private Boolean testeUm(string password)
+        {
+            string path = RYpath;
+
+            StreamReader sr = new StreamReader(path);
+            for (int x = 0; x < 14344391; x++)
+            {
+
+                string linha = sr.ReadLine();
+                if (linha.Equals(password))
+                {
+                    return false;
+                    break;
+                }
+
+            }
+            return true;
+
+        } // Lista Rockyou
+
+        private Boolean testeDois(string password)
+        {
+            bool cond = false;
+            for (int x = 0; x < password.Length; x++)
+            {
+                String letra = password[x].ToString();
+                if (Char.IsLetter(password[x]) && letra.Equals(letra.ToUpper()))
+                {
+                    cond = true;
+                    break;
+
+                }
+
+            }
+
+            return cond;
+        } // Maíscula
+
+        private Boolean testeTres(string password)
+        {
+            bool cond = false;
+            for (int x = 0; x < password.Length; x++)
+            {
+
+                if (char.IsDigit(password[x]))
+                {
+                    cond = true;
+                    break;
+                }
+            }
+            return cond;
+        } // Numero
+
+        private Boolean testeQuatro(string password)
+        {
+            for (int x = 0; x < password.Length; x++)
+            {
+                if (!Char.IsLetterOrDigit(password[x]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }// Char especial
+
+        private Boolean testeCinco(string password)
+        {
+            if (password.Length >= 10)
+            {
+                return true;
+            }
+            return false;
+        } // 10+ caracteres
+
+        private void updateCheckColors(bool[] testes)
+        {
+            
+
+            for (int x = 0; x < testes.Length; x++)
+            {
+                if (testes[x] == true)
+                {
+                    cores[x].BackColor = Color.Lime;
+                    
+                }
+                else
+                {
+                    cores[x].BackColor = Color.Red;
+                }
+            }
+
+            
+        }// Update das cores
+
+        private void afterTest(bool[] testes)
+        {
+            int counter = 0;
+
+
+            for (int x = 0; x < 5; x++)
+            {
+                if (testes[x] == true && testesCheck[x] == 0)
+                {
+                    selectText(x);
+
+                    cores[x].BackgroundImage = imgs[x];
+                    testesCheck[x] = 1;
+                }
+                if (testes[x] == true)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 5 && testesCheck[5]==0)
+            {
+                readMessage(29, 35);
+                testesCheck[5] = 1;
+            }
+        }
+
+        private void selectText(int test)
+        {
+            switch (test + 1)
+            {
+                case 1:
+                    readMessage(9, 11);
+                    break;
+                case 2:
+                    readMessage(16, 18);
+                    break;
+                case 3:
+                    readMessage(19, 21);
+                    break;
+                case 4:
+                    readMessage(22, 24);
+                    break;
+                case 5:
+                    readMessage(25, 28);
+                    break;
+            }
+        }
+
+        private void readMessage(int start, int end)
+        {
+            for (int x = start; x <= end; x++)
+            {
+                MessageBox.Show(mensagens[x] + "");
+            }
+        }
+
+        private static String[] loadText()
+        {
+
+            string path = Application.StartupPath + "\\texts.txt";
+            StreamReader sr = new StreamReader(path);
+            string[] text = new string[39];
+            for (int x = 1; x < 39; x++)
+            {
+                text[x] = sr.ReadLine();
+            }
+
+            return text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (teste == false) { this.Close(); }
+            readMessage(1, 6);
+        }
+
+        private void pb_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            execTestes(tB_senha.Text);
+        }
+
+        private void pb_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
